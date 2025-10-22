a5569985da6444374c10bf027a30bf78c201c138
Vyacheslav <slavaslava.s@yandex.ru>
2025-10-22 21:57:20 +0300

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+// import App from '@/App.vue'
+import HomeView from '@/views/HomeView.vue'
+import ProductView from '@/views/ProductView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: HomeView
+    },
+    {
+      path: '/rest',
+      component: ProductView
+    }
+  ]
+})
+
+export default router
---
+++ src/views/ProductView.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { goodsArray } from "@/constants/goods";
+const getUrl = (name) => {
+    return new URL(`../assets/images/goods/${name}`, import.meta.url);
+};
+</script>
+
+<template>
+    <section class="products">
+        <div class="container">
+            <div class="products-wrapper" id="products-wrapper">
+                <router-link
+                    class="products-card"
+                    v-for="good in goodsArray"
+                    :key="good.id"
+                    :to="`/good?id=${good.id}`"
+                >
+                    <div class="products-card__image">
+                        <img
+                            :src="getUrl(good.image)"
+                            alt="roll-ugor-standart"
+                        />
+                    </div>
+                    <div class="products-card__description">
+                        <div class="products-card__description-row">
+                            <h5 class="products-card__description--name">
+                                {{ good.title }}
+                            </h5>
+                        </div>
+                        <div class="products-card__description-row">
+                            <p class="products-card__description--text">
+                                {{ good.description }}
+                            </p>
+                        </div>
+                        <div class="products-card__description-row">
+                            <div class="products-card__description-controls">
+                                <button class="btn btn-primary">
+                                    В корзину
+                                    <img
+                                        src="../assets/images/icons/shopping-cart-white.svg"
+                                        alt="shopping-cart-white"
+                                    />
+                                </button>
+                                <span
+                                    class="products-card__description-controls--price"
+                                    >{{ good.price }}</span
+                                >
+                            </div>
+                        </div>
+                    </div>
+                </router-link>
+            </div>
+        </div>
+    </section>
+</template>
